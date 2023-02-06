d2a934f40a92b4a3fea688818aedbc173551d658
yassa <yassatamer99@gmail.com>
2023-02-06 13:28:47 +0200

    adding rock build test

---
+++ piece/rock/build/CMakeFiles/rock_test.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/yassa/CLionProjects/chess_game/piece/piece.cpp" "CMakeFiles/rock_test.dir/Users/yassa/CLionProjects/chess_game/piece/piece.cpp.o" "gcc" "CMakeFiles/rock_test.dir/Users/yassa/CLionProjects/chess_game/piece/piece.cpp.o.d"
+  "/Users/yassa/CLionProjects/chess_game/piece/rock/rock.cpp" "CMakeFiles/rock_test.dir/rock.cpp.o" "gcc" "CMakeFiles/rock_test.dir/rock.cpp.o.d"
+  "/Users/yassa/CLionProjects/chess_game/piece/rock/rock_test.cpp" "CMakeFiles/rock_test.dir/rock_test.cpp.o" "gcc" "CMakeFiles/rock_test.dir/rock_test.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ piece/rock/build/CMakeFiles/rock_test.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rock_test.dir/Users/yassa/CLionProjects/chess_game/piece/piece.cpp.o"
+  "CMakeFiles/rock_test.dir/Users/yassa/CLionProjects/chess_game/piece/piece.cpp.o.d"
+  "CMakeFiles/rock_test.dir/rock.cpp.o"
+  "CMakeFiles/rock_test.dir/rock.cpp.o.d"
+  "CMakeFiles/rock_test.dir/rock_test.cpp.o"
+  "CMakeFiles/rock_test.dir/rock_test.cpp.o.d"
+  "rock_test"
+  "rock_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rock_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
